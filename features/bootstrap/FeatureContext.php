311c0ace484b0d78f032362abc1c38f84c3c1903
Michael Murphy <michaelmurphy@appnovation.com>
2016-06-10 16:39:32 -0700

    Initial setup

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,50 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\Step\Then,
+    Behat\Behat\Context\Step\When,
+    Behat\Behat\Context\Step\Given;
+use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\MinkExtension\Context\MinkContext,
+    OrangeDigital\BusinessSelectorExtension\Context\BusinessSelectorContext;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends DrupalContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+      $this->useContext('BusinessSelectors', new BusinessSelectorContext($parameters));
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+}
